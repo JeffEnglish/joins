e05f20ec19808892bfa7f7bdfa6fc0b5480eb9a7
Jeff English <jeff@utmostlogic.com>
2014-02-05 14:00:34 -0600

    Replaced tabs w/spaces

--- examples/allJoins.js
+++ examples/allJoins.js
@@ -1,43 +1,43 @@
 var joins = require('../joins');
 
 (function testJoins() {
-	var people = [
-		{ name: 'Joe Smith', accountId: 1 },
-		{ name: 'Julie Jones', accountId: 2 },
-		{ name: 'Mark Davis', accountId: 3 }
-	];
+    var people = [
+        { name: 'Joe Smith', accountId: 1 },
+        { name: 'Julie Jones', accountId: 2 },
+        { name: 'Mark Davis', accountId: 3 }
+    ];
 
-	var property = [
-		{ pid: 1, loc: '123 Main Street' },
-		{ pid: 3, loc: '45 West Third' },
-		{ pid: 5, loc: '917 Gothard Road' },
-		{ pid: 3, loc: '1700 Avenue K Apt 3' },
-		{ pid: 4, loc: '277 Highway 210' }
-	];
+    var property = [
+        { pid: 1, loc: '123 Main Street' },
+        { pid: 3, loc: '45 West Third' },
+        { pid: 5, loc: '917 Gothard Road' },
+        { pid: 3, loc: '1700 Avenue K Apt 3' },
+        { pid: 4, loc: '277 Highway 210' }
+    ];
 
-	var result;
+    var result;
 
-	var allJoins = [
-		'innerJoin',
-		'outerJoin',
-		'leftJoin',
-		'rightJoin',
-		'leftExcludingJoin',
-		'rightExcludingJoin',
-		'outerExcludingJoin'
-	];
+    var allJoins = [
+        'innerJoin',
+        'outerJoin',
+        'leftJoin',
+        'rightJoin',
+        'leftExcludingJoin',
+        'rightExcludingJoin',
+        'outerExcludingJoin'
+    ];
 
-	allJoins.forEach(function(j) {
-		console.log(j);
-		result = joins[j](people, property, 'accountId', 'pid', function(peop, prop) {
-			return {
-				accountId: peop.accountId,
-				propertId: prop.pid,
-				accountRep: peop.name,
-				address: prop.loc
-			};
-		});
-		console.log(JSON.stringify(result, null, '\t'));
-	});
+    allJoins.forEach(function(j) {
+        console.log(j);
+        result = joins[j](people, property, 'accountId', 'pid', function(peop, prop) {
+            return {
+                accountId: peop.accountId,
+                propertId: prop.pid,
+                accountRep: peop.name,
+                address: prop.loc
+            };
+        });
+        console.log(JSON.stringify(result, null, '\t'));
+    });
 })();
 
--- joins.js
+++ joins.js
@@ -1,17 +1,17 @@
 /*
-	Utility functions for joining two arrays, including support for non-unique keys.
+    Utility functions for joining two arrays, including support for non-unique keys.
 
-		innerJoin - Returns all records in the left array that have a matching record in the right array
-		outerJoin - Returns all records in both the left and right array, matching records if they exist
-		leftJoin - Returns all records in the left array along with any matching records in the right array
-		rightJoin - Returns all records in the right array along with any matching records in the left array
-		leftExcludingJoin - Returns all records in the left array that do NOT match any records in the right array
-		rightExcludingJoin - Returns all records in the right array that do NOT match any records in the left array
-		outerExcludingJoin - Returns all records in the left and right array that do not match
+        innerJoin - Returns all records in the left array that have a matching record in the right array
+        outerJoin - Returns all records in both the left and right array, matching records if they exist
+        leftJoin - Returns all records in the left array along with any matching records in the right array
+        rightJoin - Returns all records in the right array along with any matching records in the left array
+        leftExcludingJoin - Returns all records in the left array that do NOT match any records in the right array
+        rightExcludingJoin - Returns all records in the right array that do NOT match any records in the left array
+        outerExcludingJoin - Returns all records in the left and right array that do not match
 
-	References:
-		http://www.codeproject.com/Articles/33052/Visual-Representation-of-SQL-Joins
-		http://stackoverflow.com/questions/17500312/is-there-some-way-i-can-join-the-contents-of-two-javascript-arrays-much-like-i
+    References:
+        http://www.codeproject.com/Articles/33052/Visual-Representation-of-SQL-Joins
+        http://stackoverflow.com/questions/17500312/is-there-some-way-i-can-join-the-contents-of-two-javascript-arrays-much-like-i
  */
 
 /**
@@ -24,31 +24,31 @@
  * @return {Array} array of joined records
  */
 function leftIterate(left, right, leftKey, rightKey, select) {
-	var index = [], result = [],
-		len, i, row, record, key, entries;
+    var index = [], result = [],
+        len, i, row, record, key, entries;
 
-	// Create index of right-hand records taking into account that there could be duplicate keys.
-	// Each entry in the index is an array of right-hand records matching the index key value
-	for (i = 0, len = right.length; i < len; i++) {
-		row = right[i];
-		key = row[rightKey];
-		index[key] = index[key] || [];
-		index[key].push(row);
-	}
+    // Create index of right-hand records taking into account that there could be duplicate keys.
+    // Each entry in the index is an array of right-hand records matching the index key value
+    for (i = 0, len = right.length; i < len; i++) {
+        row = right[i];
+        key = row[rightKey];
+        index[key] = index[key] || [];
+        index[key].push(row);
+    }
 
-	// Iterate the left-hand records and callback for each matching pair
-	for (i = 0, len = left.length; i < len; i++) {
-		row = left[i];
-		entries = index[row[leftKey]] || [ null ];
-		entries.forEach(function(entry) {
-			record = select(row, entry);
-			if (record) {
-				result.push(record);
-			}
-		});
-	}
+    // Iterate the left-hand records and callback for each matching pair
+    for (i = 0, len = left.length; i < len; i++) {
+        row = left[i];
+        entries = index[row[leftKey]] || [ null ];
+        entries.forEach(function(entry) {
+            record = select(row, entry);
+            if (record) {
+                result.push(record);
+            }
+        });
+    }
 
-	return result;
+    return result;
 }
 
 /**
@@ -61,21 +61,21 @@ function leftIterate(left, right, leftKey, rightKey, select) {
  * @return {Array} array of joined records
  */
 function leftRightIterate(left, right, leftKey, rightKey, select) {
-	// Iterate the left-hand array
-	var leftResult = leftIterate(left, right, leftKey, rightKey, function(left, right) {
-		return select(left, right);
-	});
+    // Iterate the left-hand array
+    var leftResult = leftIterate(left, right, leftKey, rightKey, function(left, right) {
+        return select(left, right);
+    });
 
-	// Iterate the right-hand array but only join if there is no match on the left
-	var rightResult = leftIterate(right, left, rightKey, leftKey, function(right, left) {
-		if (right && !left) {
-			return select(left, right);
-		}
-	});
+    // Iterate the right-hand array but only join if there is no match on the left
+    var rightResult = leftIterate(right, left, rightKey, leftKey, function(right, left) {
+        if (right && !left) {
+            return select(left, right);
+        }
+    });
 
-	// Append the right to the left and return
-	leftResult.push.apply(leftResult, rightResult);
-	return leftResult;
+    // Append the right to the left and return
+    leftResult.push.apply(leftResult, rightResult);
+    return leftResult;
 }
 
 /**
@@ -88,11 +88,11 @@ function leftRightIterate(left, right, leftKey, rightKey, select) {
  * @return {Array} array of joined records
  */
 exports.innerJoin = function(left, right, leftKey, rightKey, merge) {
-	return leftIterate(left, right, leftKey, rightKey, function(left, right) {
-		if (left && right && (left[leftKey] == right[rightKey])) {
-			return merge(left, right);
-		}
-	});
+    return leftIterate(left, right, leftKey, rightKey, function(left, right) {
+        if (left && right && (left[leftKey] == right[rightKey])) {
+            return merge(left, right);
+        }
+    });
 };
 
 /**
@@ -105,9 +105,9 @@ exports.innerJoin = function(left, right, leftKey, rightKey, merge) {
  * @return {Array} array of joined records
  */
 exports.outerJoin = function(left, right, leftKey, rightKey, merge) {
-	return leftRightIterate(left, right, leftKey, rightKey, function(left, right) {
-		return merge(left || {}, right || {});
-	});
+    return leftRightIterate(left, right, leftKey, rightKey, function(left, right) {
+        return merge(left || {}, right || {});
+    });
 };
 
 /**
@@ -120,9 +120,9 @@ exports.outerJoin = function(left, right, leftKey, rightKey, merge) {
  * @return {Array} array of joined records
  */
 exports.leftJoin = exports.leftOuterJoin = function(left, right, leftKey, rightKey, merge) {
-	return leftIterate(left, right, leftKey, rightKey, function(left, right) {
-		return merge(left, right || {});
-	});
+    return leftIterate(left, right, leftKey, rightKey, function(left, right) {
+        return merge(left, right || {});
+    });
 };
 
 /**
@@ -135,9 +135,9 @@ exports.leftJoin = exports.leftOuterJoin = function(left, right, leftKey, rightK
  * @return {Array} array of joined records
  */
 exports.rightJoin = exports.rightOuterJoin = function(left, right, leftKey, rightKey, merge) {
-	return leftIterate(right, left, rightKey, leftKey, function(right, left) {
-		return merge(left || {}, right);
-	});
+    return leftIterate(right, left, rightKey, leftKey, function(right, left) {
+        return merge(left || {}, right);
+    });
 };
 
 /**
@@ -150,11 +150,11 @@ exports.rightJoin = exports.rightOuterJoin = function(left, right, leftKey, righ
  * @return {Array} array of joined records
  */
 exports.leftExcludingJoin = function(left, right, leftKey, rightKey, merge) {
-	return leftIterate(left, right, leftKey, rightKey, function(left, right) {
-		if (left && !right) {
-			return merge(left, {});
-		}
-	});
+    return leftIterate(left, right, leftKey, rightKey, function(left, right) {
+        if (left && !right) {
+            return merge(left, {});
+        }
+    });
 };
 
 /**
@@ -167,11 +167,11 @@ exports.leftExcludingJoin = function(left, right, leftKey, rightKey, merge) {
  * @return {Array} array of joined records
  */
 exports.rightExcludingJoin = function(left, right, leftKey, rightKey, merge) {
-	return leftIterate(right, left, rightKey, leftKey, function(right, left) {
-		if (right && !left) {
-			return merge({}, right);
-		}
-	});
+    return leftIterate(right, left, rightKey, leftKey, function(right, left) {
+        if (right && !left) {
+            return merge({}, right);
+        }
+    });
 };
 
 /**
@@ -184,9 +184,9 @@ exports.rightExcludingJoin = function(left, right, leftKey, rightKey, merge) {
  * @return {Array} array of joined records
  */
 exports.outerExcludingJoin = function(left, right, leftKey, rightKey, merge) {
-	return leftRightIterate(left, right, leftKey, rightKey, function(left, right) {
-		if (!left || !right) {
-			return merge(left || {}, right || {});
-		}
-	});
+    return leftRightIterate(left, right, leftKey, rightKey, function(left, right) {
+        if (!left || !right) {
+            return merge(left || {}, right || {});
+        }
+    });
 };
